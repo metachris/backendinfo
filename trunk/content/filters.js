9be2bd58a3544833be1c6014d92a1740dc0d8110
hager@linuxuser.at <hager@linuxuser.at@16126426-ea1a-11dd-b00f-9fabb4b2ff13>
2009-01-24 20:46:26 +0000

    conditional filters (applied only if another filter is found)

--- trunk/content/filters.js
+++ trunk/content/filters.js
@@ -18,11 +18,57 @@ filters.push({
     )
 });
 
+/* === WORDPRESS 2.x Base Filter ===*/
 filters.push({
-    name: "Wordpress",
+    name: "Wordpress 2.x",
     image: "chrome://backendinfo/skin/wordpress.png",
     require: new Array(
-        { url: '/wp-admin', contains: new Array('wp-admin') },
-        { url: '/', contains: new Array('wp-content', 'wp-includes') }
+        { url: '/', contains: new Array('wp-content/themes', 'xmlrpc.php') },
+        { url: '/wp-admin', contains: new Array('wp-admin') }
     )
 });
+
+/* === WORDPRESS 2.x Conditional Filters === */
+    filters.push({
+        name: "Wordpress 2.7",
+        parent: "Wordpress 2.x",
+        require: new Array(
+            { url: '/readme.html', contains: new Array('wp-admin', 'Version 2.7') }
+        )
+    });
+    filters.push({
+        name: "Wordpress 2.6",
+        parent: "Wordpress 2.x",
+        require: new Array(
+            { url: '/readme.html', contains: new Array('wp-admin', 'Version 2.6') }
+        )
+    });
+    filters.push({
+        name: "Wordpress 2.5",
+        parent: "Wordpress 2.x",
+        require: new Array(
+            { url: '/readme.html', contains: new Array('wp-admin', 'Version 2.5') }
+        )
+    });
+    filters.push({
+        name: "Wordpress 2.3",
+        parent: "Wordpress 2.x",
+        require: new Array(
+            { url: '/readme.html', contains: new Array('wp-admin', 'Version 2.3') }
+        )
+    });
+    filters.push({
+        name: "Wordpress 2.2",
+        parent: "Wordpress 2.x",
+        require: new Array(
+            { url: '/readme.html', contains: new Array('wp-admin', 'Version 2.2') }
+        )
+    });
+    filters.push({
+        name: "Wordpress 2.1",
+        parent: "Wordpress 2.x",
+        require: new Array(
+            { url: '/readme.html', contains: new Array('wp-admin', 'Version 2.1') }
+        )
+    });
+
