80978f75fa1aa96b971b000f90e8a6ca8d90c494
hager@linuxuser.at <hager@linuxuser.at@16126426-ea1a-11dd-b00f-9fabb4b2ff13>
2009-01-25 19:51:56 +0000

    good filters for drupal 6.x

--- trunk/content/filters_drupal.js
+++ trunk/content/filters_drupal.js
@@ -6,6 +6,87 @@ filters.push({
         { url: '/',  contains: new Array('modules/system/system.css?', 'modules/node/node.css?', 'modules/user/user.css?') }
     )
 });
+/* === Drupal 6.x Conditional Filters === */
+    filters.push({
+        name: "Drupal 6.9",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.9"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.22 2009/01/14') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.8",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.8"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.20 2008/12/11') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.7",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.7"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.18 2008/12/10') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.6",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.6"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.15 2008/10/22') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.5",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.5"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.13 2008/10/08') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.4",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.4"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.11 2008/08/13') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.3",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.3"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.9 2008/07/09') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.2",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.2"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.7 2008/04/09') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.1",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.1"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.5 2008/02/27') }
+        )
+    });
+    filters.push({
+        name: "Drupal 6.0",
+        parent: "Drupal 6.x",
+        require: new Array(
+            { url: '/modules/taxonomy/taxonomy.info', contains: new Array('project = "drupal"', 'version = "6.0"') },
+            { url: '/CHANGELOG.txt', contains: new Array('1.253.2.3 2008/02/13') }
+        )
+    });
 
 /* Drupal 5: Uses always the @import statement */
 filters.push({
@@ -16,7 +97,7 @@ filters.push({
     )
 });
 
-/* === DRUPAL 5.x Conditional Filters === */
+/* === Drupal 5.x Conditional Filters === */
     filters.push({
         name: "Drupal 5.9",
         parent: "Drupal 5.x",
