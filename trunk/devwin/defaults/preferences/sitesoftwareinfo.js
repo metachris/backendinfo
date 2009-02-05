7d4e38c0aa84e760580ccb9ef3f272361ee2ffa6
metakaram <metakaram@16126426-ea1a-11dd-b00f-9fabb4b2ff13>
2009-02-05 01:58:01 +0000

    overwrite old trunk with devwin branch

---
+++ trunk/devwin/defaults/preferences/sitesoftwareinfo.js
@@ -0,0 +1,5 @@
+pref("extensions.backendinfo.boolpref", false);
+pref("extensions.backendinfo.loadicon", true);
+pref("extensions.backendinfo.statustext", true);
+pref("extensions.backendinfo.testurl", "");
+pref("extensions.backendinfo.testfilters", "");
